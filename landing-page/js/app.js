55429bb5f2e46acffae5ab3ad492d7070a04fa87
Jumana <jumana.almahmoud@gmail.com>
2020-06-12 03:27:16 -0400

    to be submitted

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,127 +1,60 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+const navbar = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
 
-/**
- * Define Global Variables
- * 
-*/
-const navbar= document.getElementById("navbar__list");
-const sections= document.querySelectorAll("section");
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-function createList(){
-    const list_elements=[];
+function createList() {
+  const list_elements = [];
 
-    for (let index = 0; index < sections.length; index++) {
-        let list_element ={};
-        list_element.text=sections[index].dataset.nav;
-        list_element.id=sections[index].id;
-        list_elements.push(list_element);
-    }
-    return list_elements;
-    console.log("createList()");
-    console.log(list_elements);
-    
-    
+  for (let index = 0; index < sections.length; index++) {
+    let list_element = {};
+    list_element.text = sections[index].dataset.nav;
+    list_element.id = sections[index].id;
+    list_elements.push(list_element);
+  }
+  return list_elements;
 }
 
-function createNavElements(list_elements){
-    let elements=[];
-    for (let index = 0; index < list_elements.length; index++) {
-        let link = document.createElement("a");
-        let li = document.createElement("li");
-        link.setAttribute("href", "#"+list_elements[index].id);
-        link.innerHTML = list_elements[index].text;
-        li.append(link);
-        elements.push(li);
-    }
-      return elements;
-      console.log("creatNavElements()");
-      console.log(elements);
-      
-        
-    }
-
-
-/**
- * End Helper Functions
- 
-
-
- * Begin Main Functions
- * 
-*/
+function createNavElements(list_elements) {
+  let elements = [];
+  for (let index = 0; index < list_elements.length; index++) {
+    let link = document.createElement("a");
+    let li = document.createElement("li");
+    link.setAttribute("href", "#" + list_elements[index].id);
+    link.innerHTML = list_elements[index].text;
+    li.append(link);
+    elements.push(li);
+  }
+  return elements;
+}
 
 // build the nav
 function buildNav() {
-    let nav_elements = createNavElements(createList());
-    createActiveEventListeners(nav_elements);
-    navbar.append(... nav_elements);
-    console.log("buildNav()");
-    console.log(nav_elements);
-    
-    
-    
+  let nav_elements = createNavElements(createList());
+  createActiveEventListeners(nav_elements);
+  navbar.append(...nav_elements);
 }
 
+function createActiveEventListeners(nav_elements) {
+  let top, left;
 
-// Add class 'active' to section when near top of viewport
-function createActiveEventListeners(nav_elements){
-    console.log("inside createActiveEventlisteners");
-    console.log(sections.length)
-    
-for (let index = 0; index < nav_elements.length; index++) {
-    console.log("in the loop");
-    nav_elements[index].addEventListener("click", function(){  
-        sections.forEach(function(section) {
-            section.removeAttribute("class");
-          })
+  for (let index = 0; index < nav_elements.length; index++) {
+    nav_elements[index].addEventListener("click", function (e) {
+      e.preventDefault();
 
-        sections[index].setAttribute("class","active-class")
-        console.log("in the event: " + index);
-        
-        
-       });
-    } 
-    
+      sections.forEach(function (section) {
+        section.removeAttribute("class");
+      });
+
+      sections[index].setAttribute("class", "active-class");
+      top = sections[index].offsetTop;
+      left = sections[index].offsetLeft;
+      window.scrollTo({
+        top: top,
+        left: left,
+        behavior: "smooth",
+      });
+    });
+  }
 }
 
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-
+// the following will call the main function
 buildNav();
-
-/**
- * End Main Functions
- 
-
-
-
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
